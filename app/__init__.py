15ba873ce39396877a3d167da5d77506d7b2328c
Ezague <kristian.petersen@lego.com>
2022-10-07 10:49:36 +0200

    Fully functioning API backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -9,11 +10,13 @@ db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 jwt = JWTManager()
+cors = CORS()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
+    cors.init_app(app)
     jwt.init_app(app)
     login.init_app(app)
     db.init_app(app)
